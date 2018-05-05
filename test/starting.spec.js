b3ce9d01ac01d7b5b293417a5b7ed406dbe250b5
YoYea <yuttasakcom@gmail.com>
2018-05-05 11:21:12 +0700

    testing objects

--- test/starting.spec.js
+++ test/starting.spec.js
@@ -1,7 +1,11 @@
 const assert = require("assert");
 
 describe("Basic Mocha Test", () => {
-  it("should throw errs", () => {
-    assert.equal(3, 3);
+  it("should deal with objects", () => {
+    const obj = { name: "YoYea", gender: "male" };
+    const obj2 = { name: "YoYea", gender: "male" };
+
+    obj.should.have.property("name").equal("YoYea");
+    obj.should.deep.equal(obj2);
   });
 });
